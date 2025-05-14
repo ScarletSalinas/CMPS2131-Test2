fef4e0146432cfce0ea480c4ec3626c3f61f4ecc
Scarlet Salinas <2022156127@ub.edu.bz>
2025-05-14 14:25:23 -0600

    added: hashtable class + methods

---
+++ hashtable.h
@@ -0,0 +1,27 @@
+#ifndef HASHTABLE_H 
+#define HASHTABLE_H
+
+#include <vector>
+#include <iostream>
+
+#define TABLE_SIZE 10 // Size of the hash table
+
+class HashTable {
+private:
+    std::vector<int> table; // Vector to store keys (-1 = empty slot)
+
+public:
+    // Constructor
+    HashTable();
+
+    // Hash function (Division Method)
+    int hash(int key);
+
+    // Insert key with collision resolution (Linear Probing)
+    void insert(int key);
+
+    // Display the hash table contents
+    void display();
+};
+
+#endif // HASHTABLE_H
